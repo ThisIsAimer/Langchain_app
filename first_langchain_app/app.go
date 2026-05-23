7755739e3c03e66d18652a5e5a73a8f52d40b229
raunak <raunakbiswas05@gmail.com>
2026-05-23 19:47:21 +0530

    with gemini

--- first_langchain_app/app.go
+++ first_langchain_app/app.go
@@ -3,29 +3,38 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/llms/googleai"
 )
 
 func main() {
+
+	ctx := context.Background()
 
 	err := godotenv.Load(`first_langchain_app/.env`)
 	if err != nil {
 		log.Fatal(err)
 
 	}
+	key := os.Getenv("GOOGLEAI_API_KEY")
+	// llm, err := openai.New()
+	llm, err := googleai.New(
+		ctx,
+		googleai.WithAPIKey(key),
+		googleai.WithDefaultModel("gemini-2.5-flash"),
+	)
 
-	llm, err := openai.New()
 	if err != nil {
 		log.Fatal(err)
 
 	}
 
-	prompt := "why is golang so good?"
+	prompt := "whats the capital of india?"
 
-	completion, err := llms.GenerateFromSinglePrompt(context.Background(), llm, prompt)
+	completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
 	if err != nil {
 		log.Fatal(err)
 
